ec42e131ad0d20cf7e5c219b58bf09bd2929ae52
Prashant <prashantkaushik11.pk@gmail.com>
2020-04-05 16:57:00 +0530

    environment setup part 2

--- src/main/java/com/josh/First.java
+++ src/main/java/com/josh/First.java
@@ -1,7 +1,12 @@
 package com.josh;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class First {
-    public void myName(){
-        System.out.println("1");
+    @RequestMapping("/")
+    public String myName(){
+        return "index";
     }
 }
